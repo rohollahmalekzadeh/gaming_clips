ce72523d11e5aea0523fc4d103169eeb93c65739
Rohollah <rohollah.malekzadeh@gmail.com>
2022-10-19 16:22:09 +0200

    feat: add modal service

---
+++ src/app/core/services/modal/modal.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core'
+import {modal} from 'src/app/core/enums/modal.enum'
+
+interface IModal {
+  id: string
+  visible: boolean
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private modals: IModal[] = []
+
+  constructor() {}
+
+  register(id: modal) {
+    const existingModal = !this.modals.find(modal => modal.id === id)
+    if (existingModal) return
+
+    this.modals.push({id, visible: false})
+  }
+
+  unregister(id: modal) {
+    this.modals = this.modals.filter(modal => modal.id !== id)
+  }
+
+  toggle(id: modal) {
+    const modal = this.modals.find(modal => modal.id === id)
+
+    if (modal) modal.visible = !modal?.visible
+  }
+
+  isModalOpen(id: modal) {
+    return !!this.modals.find(modal => modal.id === id)?.visible
+  }
+}
