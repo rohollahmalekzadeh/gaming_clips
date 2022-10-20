fde7cab68c28c09855108cb1cd39fbf78737651f
Rohollah <rohollah.malekzadeh@gmail.com>
2022-10-20 23:45:56 +0200

    feat: add video routing module

---
+++ src/app/features/video/video-routing.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
+
+import {
+  AngularFireAuthGuard,
+  redirectUnauthorizedTo,
+} from '@angular/fire/compat/auth-guard'
+
+import {UploadComponent} from './upload/upload.component'
+import {ManageComponent} from './manage/manage.component'
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/')
+
+const routes: Routes = [
+  {
+    path: 'upload',
+    component: UploadComponent,
+    data: {authOnly: true, authGuardPipe: redirectUnauthorizedToHome},
+    // canActivate: [AngularFireAuthGuard],
+  },
+  {
+    path: 'manage',
+    component: ManageComponent,
+    data: {authOnly: true, authGuardPipe: redirectUnauthorizedToHome},
+    // canActivate: [AngularFireAuthGuard],
+  },
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class VideoRoutingModule {}
--- src/app/features/video/video.module.ts
+++ src/app/features/video/video.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
+
+import {VideoRoutingModule} from './video-routing.module'
 import {SharedModule} from 'src/app/shared/shared.module'
+import {UploadComponent} from './upload/upload.component'
+import {ManageComponent} from './manage/manage.component'
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, SharedModule],
+  declarations: [UploadComponent, ManageComponent],
+  imports: [CommonModule, SharedModule, VideoRoutingModule],
   exports: [],
 })
 export class VideoModule {}
