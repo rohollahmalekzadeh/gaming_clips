143fe44ad9612eebbe945009a591a317336e90fd
Rohollah <rohollah.malekzadeh@gmail.com>
2022-10-20 19:33:55 +0200

    feat: add safelist to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
+  safelist: [
+    '-top-1',
+    'left-3',
+    'text-sm',
+    'opacity-100',
+    'bg-secondary',
+    'bg-red-400',
+    'bg-blue-400',
+    'bg-green-400',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
