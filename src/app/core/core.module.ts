4cafa966f06fb59bded499ad3256e422694136c2
Rohollah <rohollah.malekzadeh@gmail.com>
2022-10-20 23:49:29 +0200

    feat: add clip model

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,15 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
+import {RouterModule} from '@angular/router'
+
+import {NavbarComponent} from '../core/components/navbar/navbar.component'
+
+import {SharedModule} from '../shared/shared.module'
+import {AuthenticationModule} from '../features/authentication/authentication.module'
 
 @NgModule({
-  imports: [CommonModule],
+  declarations: [NavbarComponent],
+  imports: [CommonModule, SharedModule, RouterModule, AuthenticationModule],
+  exports: [NavbarComponent],
 })
 export class CoreModule {}
