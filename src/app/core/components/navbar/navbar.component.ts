c0965c33211d1e55721ca618c01e21de3dd1ceba
Rohollah <rohollah.malekzadeh@gmail.com>
2022-10-20 23:52:12 +0200

    feat: add logout func and scroll event to navbar

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnInit, OnDestroy} from '@angular/core'
 import {ModalService} from '../../services/modal/modal.service'
+import {AuthService} from '../../services/authentication/auth.service'
 
 import {modal} from '../../enums/enum-modal'
 
@@ -8,13 +9,36 @@ import {modal} from '../../enums/enum-modal'
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
   modal = modal
-  constructor(public modalService: ModalService) {}
+  opacity = false
+  constructor(
+    public modalService: ModalService,
+    public authService: AuthService,
+  ) {}
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
 
   openModal($event: Event) {
     $event.preventDefault()
 
     this.modalService.toggle(modal.AUTH)
+  }
+
+  logout($event: Event) {
+    this.authService.logout($event)
+  }
+
+  handleScroll = () => {
+    const {scrollTop} = document.documentElement
+    console.log(scrollTop)
+
+    this.opacity = scrollTop > 100 ? true : false
   }
 }
