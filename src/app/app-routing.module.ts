f1583602dcef6857697145c1d51b987365c5d1fc
Rohollah_Malekzadeh@gmail.com <rohollah.malekzadeh@gmail.com>
2022-10-23 15:08:01 +0200

    feat: add clip route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
+import {ClipService} from './core/services/clip/clip.service'
 
 import {AboutUsComponent} from './features/about-us/about-us.component'
+import {ClipComponent} from './features/clip/clip.component'
 import {HomeComponent} from './features/home/home.component'
 import {NotFoundComponent} from './features/not-found/not-found.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutUsComponent},
+  {path: 'clip/:id', component: ClipComponent, resolve: {clip: ClipService}},
+
+  {
+    path: '',
+    loadChildren: async () =>
+      (await import('./features/video/video.module')).VideoModule,
+  },
   {path: '**', component: NotFoundComponent},
 ]
 
