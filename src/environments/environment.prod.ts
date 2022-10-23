c55a3d7830cd9380c80e06d1b921c2867449fbac
Rohollah_Malekzadeh@gmail.com <rohollah.malekzadeh@gmail.com>
2022-10-23 15:29:50 +0200

    chore: add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAl3-juYiljg7RbFQuWe2OJxPoGPgjU6Sk',
+    authDomain: 'clips-83156.firebaseapp.com',
+    projectId: 'clips-83156',
+    storageBucket: 'clips-83156.appspot.com',
+    messagingSenderId: '318569526856',
+    appId: '1:318569526856:web:e7d4b37828073277f50a7d',
+  },
+}
